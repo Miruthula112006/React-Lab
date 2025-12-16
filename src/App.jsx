43d6ec67da4a2f7a8b4cf2e85db635cb14c58577
Miruthula112006 <vmiruthula11@gmail.com>
2025-12-16 12:05:18 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App(){
+ return (
+  <>
+   <h1>vishwa</h1>
+   <h2>sanjay</h2>
+  </>
+ ) 
+}
+
+export default App
